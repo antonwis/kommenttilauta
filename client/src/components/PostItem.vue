8aca3d1a5f26d371268b21497d14e7748e60e858
antonwis <56794229+antonwis@users.noreply.github.com>
2019-12-10 09:38:11 +0200

    added: like button, comment delete button

--- client/src/components/PostItem.vue
+++ client/src/components/PostItem.vue
@@ -3,7 +3,9 @@
     <div class="message-header">
       <p>{{ post.title }}</p>
  
+      <button @click="addLike" class="like_button" aria-label="like">Like</button> 
       <button @click="deletePost" class="delete" aria-label="delete"></button>
+      
     </div>
     <div class="message-body">
       {{ post.body }}
@@ -14,17 +16,19 @@
       <span class="has-text-grey-light">last updated: {{ post.updatedAt | moment }}</span><br>
       <UpdatePostModule :post="post" @updatePost="updatePost" :key="post._id"/>
     </div>
-    <div class="comment-section">
+    <div class="message-body">
       <div v-for="(comment, index) in comments" :comment="comment" :key="index">
         <h6 v-if="comment.name">{{ comment.name }}</h6>
         <p v-if="comment.text">{{ comment.text }}</p>
+        <button @click="deleteComment(index)" class="delete" aria-label="delete"></button>
       </div>
       <AddCommentModule :post="post" @addComment="addComment" :key="post._id"/>
+      
     </div>
   </article>
 </template>
 <script>
-  import { deletePost, addComment } from '../repository'
+  import { deletePost, addComment, deleteComment, addLike } from '../repository'
   import UpdatePostModule from './UpdatePostModule'
   import AddCommentModule from './AddCommentModule'
   import Moment from 'moment'
@@ -45,12 +49,23 @@
           .then(() => this.$emit('deletePost', this.post._id))
           .catch(err => console.log(err))
       },
+      deleteComment(index){
+        const i = index;
+         deleteComment(this.post._id, this.comments[i]._id)
+           .then(() => this.$emit('deleteComment', this.comments._id))
+           .catch(err => console.log(err));
+      },
       updatePost(post){
         this.$emit('updatePost', post)
       },
       addComment(comment) {
         this.comments.concat(comment);
         location.reload();
+      },
+      addLike(e) {
+        e.preventDefault();
+        addLike(this.post._id)
+          .catch(err => console.log(err))
       }
     },
     filters: {
@@ -59,7 +74,7 @@
       }
     },
     mounted() {
-    this.comments = this.post.comments;
+      this.comments = this.post.comments;
     }
   }
 </script>
